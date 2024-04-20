1d673e3225998dbea8e1221e6c8a47e18e590556
Tania Bezerra <nybrasil@gmail.com>
2024-04-20 15:37:40 -0400

    updated seeds.sql

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,18 +1,18 @@
 -- Insert departments
-INSERT INTO departments (departments_name) VALUES
-('Marketing'),
-('Engineering'),
-('Sales');
+INSERT INTO departments (department_name) 
+VALUES ('Marketing'), ('Engineering'), ('Sales');
 
 -- Insert roles
-INSERT INTO roles (title, salary, department_id) VALUES
-('Marketing Manager', 60000, 1),
-('Software Engineer', 80000, 2),
+INSERT INTO roles (title, salary, department_id) 
+VALUES 
+('Marketing Manager', 60000, 1), 
+('Software Engineer', 80000, 2), 
 ('Sales Representative', 50000, 3);
 
 -- Insert employees
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL),
-('Jane', 'Smith', 2, 1),
-('Alice', 'Johnson', 2, 1),
-('Bob', 'Brown', 3, 1);    
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES 
+('John', 'Doe', 1, NULL), 
+('Jane', 'Smith', 2, NULL), 
+('Alice', 'Johnson', 2, NULL), 
+('Bob', 'Brown', 3, NULL);
